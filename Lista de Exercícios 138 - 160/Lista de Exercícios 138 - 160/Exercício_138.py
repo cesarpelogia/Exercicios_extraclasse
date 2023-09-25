18f85130e48af5709afd5d46c269ce0b84dca03d
cesarpelogia <142257303+cesarpelogia@users.noreply.github.com>
2023-09-25 20:44:27 -0300

    Update Exercício_138.py

--- Lista de Exercícios 138 - 160/Lista de Exercícios 138 - 160/Exercício_138.py
+++ Lista de Exercícios 138 - 160/Lista de Exercícios 138 - 160/Exercício_138.py
@@ -10,19 +10,19 @@ elif (num == 4):
     print("\nAbril")
 elif (num == 5):
     print("\nMaio")
-elif (num == 4):
+elif (num == 6):
     print("\nJunho")
-elif (num == 4):
+elif (num == 7):
     print("\nJulho")
-elif (num == 4):
+elif (num == 8):
     print("\nAgosto")
-elif (num == 4):
+elif (num == 9):
     print("\nSetembro")
-elif (num == 4):
+elif (num == 10):
     print("\nOutubro")
-elif (num == 4):
+elif (num == 11):
     print("\nNovembro")
-elif (num == 4):
+elif (num == 12):
     print("\nDezembro")
 else:
     print("\nNão existe mês correspondente a este número.")
